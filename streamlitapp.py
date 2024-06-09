c178f46e41ca35ddea27fac1a158136a3a5c926f
xddinsd <i.a.lavrik@gmail.com>
2024-06-09 03:18:03 +0300

    Modules folder added to make project more clear

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,15 +2,19 @@
 
 import time
 import streamlit as slit
-import Cohort_Statistics
+
 from typing import List
+
+import sys
+sys.path.append("./Modules")
+import CohortStatistics
 from DfServices import DfServicesModule, Visuals
 
 def getMeanStat(grade, year, subjectName):
     # Stat for hypotesis 1
     scatterPlotFig = Visuals.makeScatterPlot(scatterData = 
         DfServicesModule.getScatterData(grade, year, subjectName))
-    stat = Cohort_Statistics.cohortStat(
+    stat = CohortStatistics.cohortStat(
         DfServicesModule.getMeanData(grade, year, subjectName))
     
     color = 'red' if grade == 10 else 'blue'
@@ -24,7 +28,7 @@ def getPercentileStat():
     # Stat for hypotesis 2
     scatterPlotFig = Visuals.makeScatterPlot(
         scatterData = DfServicesModule.getCompleteScatterData())
-    stat = Cohort_Statistics.cohortStat(
+    stat = CohortStatistics.cohortStat(
         DfServicesModule.getPercentileData())
 
     color = 'rainbow'
